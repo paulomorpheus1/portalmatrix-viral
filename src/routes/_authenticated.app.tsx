050ff9cfda8d3ae82593512667a90977ffa38aa8
paulomorpheus1 <paulormorpheus21@gmail.com>
2026-05-17 05:48:27 -0300

    Update and rename app.tsx to _authenticated.app.tsx

--- src/routes/_authenticated.app.tsx
+++ src/routes/_authenticated.app.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/app")({
+export const Route = createFileRoute("/_authenticated/app")({
   component: AppComponent,
 });
 
