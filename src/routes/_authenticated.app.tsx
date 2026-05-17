7a7a689d7442eb5f2676109f5eb4a6d10700edd4
paulomorpheus1 <paulormorpheus21@gmail.com>
2026-05-17 06:02:35 -0300

    Delete src/routes/_authenticated.app.tsx

--- src/routes/_authenticated.app.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/app")({
-  component: AppComponent,
-});
-
-function AppComponent() {
-  return <Outlet />;
-}
