5c3e92e44840e2b6b24a83749e638f8ef56df48a
paulomorpheus1 <paulormorpheus21@gmail.com>
2026-05-17 04:19:29 -0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,9 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: RootLayout,
@@ -8,9 +11,11 @@ export const Route = createRootRoute({
 
 function RootLayout() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="portal-matrix-theme">
-      <Outlet />
-      <Toaster />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="portal-matrix-theme">
+        <Outlet />
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
